b79e3deb9e0f5ce137cb38be522f07488570e1b2
Mohit Punyani <mohitpunyani916@gmail.com>
2025-07-02 23:18:57 +0530

    System Design

--- 5_solid_design_principle/ISP/code/ISP_follow.cpp
+++ 5_solid_design_principle/ISP/code/ISP_follow.cpp
@@ -1,18 +1,22 @@
-
 #include <iostream>
-#include <stdexcept>
 
 using namespace std;
 
-// Single interface for all shapes (Violates ISP)
-class Shape {
+// Separate interface for 2D shapes
+class TwoDimensionalShape {
 public:
     virtual double area() = 0;
-    virtual double volume() = 0; // 2D shapes don't have volume!
 };
 
-// Square is a 2D shape but is forced to implement volume()
-class Square : public Shape {
+// Separate interface for 3D shapes
+class ThreeDimensionalShape {
+public:
+    virtual double area() = 0;
+    virtual double volume() = 0;
+};
+
+// Square implements only the 2D interface
+class Square : public TwoDimensionalShape {
 private:
     double side;
 
@@ -22,14 +26,10 @@ public:
     double area() override {
         return side * side;
     }
-
-    double volume() override {
-        throw logic_error("Volume not applicable for Square"); // Unnecessary method
-    }
 };
 
-// Rectangle is also a 2D shape but is forced to implement volume()
-class Rectangle : public Shape {
+// Rectangle implements only the 2D interface
+class Rectangle : public TwoDimensionalShape {
 private:
     double length, width;
 
@@ -39,14 +39,10 @@ public:
     double area() override {
         return length * width;
     }
-
-    double volume() override {
-        throw logic_error("Volume not applicable for Rectangle"); // Unnecessary method
-    }
 };
 
-// Cube is a 3D shape, so it actually has a volume
-class Cube : public Shape {
+// Cube implements the 3D interface
+class Cube : public ThreeDimensionalShape {
 private:
     double side;
 
@@ -63,20 +59,14 @@ public:
 };
 
 int main() {
-    Shape* square = new Square(5);
-    Shape* rectangle = new Rectangle(4, 6);
-    Shape* cube = new Cube(3);
+    TwoDimensionalShape* square = new Square(5);
+    TwoDimensionalShape* rectangle = new Rectangle(4, 6);
+    ThreeDimensionalShape* cube = new Cube(3);
 
     cout << "Square Area: " << square->area() << endl;
     cout << "Rectangle Area: " << rectangle->area() << endl;
     cout << "Cube Area: " << cube->area() << endl;
     cout << "Cube Volume: " << cube->volume() << endl;
-
-    try {
-        cout << "Square Volume: " << square->volume() << endl; // Will throw an exception
-    } catch (logic_error& e) {
-        cout << "Exception: " << e.what() << endl;
-    }
     
     return 0;
 }
